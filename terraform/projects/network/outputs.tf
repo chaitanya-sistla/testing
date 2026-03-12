bed17899600cf51110ba771f76f7d97f9136f9ed
Chaitanya Sistla <122406662+chaitanya-sistla@users.noreply.github.com>
2026-03-12 10:50:07 -0500

    Update Terraform configuration

---
+++ terraform/projects/network/outputs.tf
@@ -0,0 +1,18 @@
+#------------------------------------------------------------------------------
+# VPC Outputs
+#------------------------------------------------------------------------------
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "vpc_cidr" {
+  description = "CIDR block of the VPC"
+  value       = aws_vpc.main.cidr_block
+}
+
+output "vpc_arn" {
+  description = "ARN of the VPC"
+  value       = aws_vpc.main.arn
+}
