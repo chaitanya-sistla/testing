d1d66fc36ff368ced32469b6224f2208add5afc4
Chaitanya Sistla <122406662+chaitanya-sistla@users.noreply.github.com>
2026-03-16 15:54:10 -0500

    Update Terraform configuration

---
+++ terraform/projects/oxid/outputs.tf
@@ -0,0 +1,81 @@
+#------------------------------------------------------------------------------
+# VPC Outputs
+#------------------------------------------------------------------------------
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+#------------------------------------------------------------------------------
+# EKS Cluster Outputs
+#------------------------------------------------------------------------------
+
+output "cluster_id" {
+  description = "ID of the EKS cluster"
+  value       = aws_eks_cluster.main.id
+}
+
+output "cluster_name" {
+  description = "Name of the EKS cluster"
+  value       = aws_eks_cluster.main.name
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for the EKS cluster API server"
+  value       = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID attached to the EKS cluster"
+  value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data for cluster authentication"
+  value       = aws_eks_cluster.main.certificate_authority[0].data
+  sensitive   = true
+}
+
+output "cluster_version" {
+  description = "Kubernetes version of the EKS cluster"
+  value       = aws_eks_cluster.main.version
+}
+
+#------------------------------------------------------------------------------
+# EKS Node Group Outputs
+#------------------------------------------------------------------------------
+
+output "node_group_id" {
+  description = "ID of the EKS node group"
+  value       = aws_eks_node_group.main.id
+}
+
+output "node_group_status" {
+  description = "Status of the EKS node group"
+  value       = aws_eks_node_group.main.status
+}
+
+output "node_group_arn" {
+  description = "ARN of the EKS node group"
+  value       = aws_eks_node_group.main.arn
+}
+
+#------------------------------------------------------------------------------
+# Connection Information
+#------------------------------------------------------------------------------
+
+output "configure_kubectl" {
+  description = "Command to configure kubectl for this cluster"
+  value       = "aws eks update-kubeconfig --region ${var.region} --name ${aws_eks_cluster.main.name}"
+}
